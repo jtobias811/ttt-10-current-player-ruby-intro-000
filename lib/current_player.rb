0571ec51e4a17ca566f29482e76f02fbd0148e2b
Jacob Tobias <jtobias811@gmail.com>
2020-10-15 14:29:44 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space = "X" || space = "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  turns = 0
+  if turn_count % 2 = 0
+    return "X"
+  else
+    return "O"
+  end
+end
